195566dc6d3ce9cff042827b3985aebdad271218
nkvdmax <84874356+nkvdmax@users.noreply.github.com.>
2025-09-12 18:29:32 +0300

    fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/anoma-intent-demo/',   // ✅ важливо для GitHub Pages
   server: { port: 5173 }
 })
